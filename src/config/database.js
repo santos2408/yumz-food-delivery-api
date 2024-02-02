5c58f98fcfd94ade9f0c364b8a5f55b40fa0ffb8
Roger Santos <roger.santos36@gmail.com>
2024-02-02 19:34:16 -0300

    fix enviroment variables

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,14 @@
 const mysql = require("mysql2");
 
+const dotenv = require("dotenv");
+
+dotenv.config()
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
   database: process.env.DB_NAME,
-  password: "",
+  password: process.env.DB_PASSWORD,
 });
 
 // module.exports = {

a2ad8954d785f0ce1ac995715c4c7f6e1540b093
Roger Santos <roger.santos36@gmail.com>
2024-02-02 18:21:31 -0300

    render config

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./src/app");
 
-const port = process.env.port || 3000;
+const port = process.env.DB_PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Yumz! Food Delivery API executando na porta: ${port}`);
--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "yumz_food_delivery",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  database: process.env.DB_NAME,
   password: "",
 });
 

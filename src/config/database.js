1148f8bad5deba610ba48b65729c907dd145b893
Roger Santos <roger.santos36@gmail.com>
2024-02-03 21:55:21 -0300

    fixing '.env' environment variables

--- src/config/database.js
+++ src/config/database.js
@@ -4,23 +4,12 @@ const dotenv = require("dotenv");
 
 dotenv.config()
 
-// const pool = mysql.createPool({
-//   host: process.env.MYSQL_ADDON_HOST,
-//   user: process.env.MYSQL_ADDON_DB_USER,
-//   database: process.env.MYSQL_ADDON_DB,
-//   password: process.env.MYSQL_ADDON_PASSWORD,
-//   port: process.env.MYSQL_ADDON_PORT,
-// });
-
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "yumz_food_delivery",
-  password: "",
-  port: process.env.MYSQL_ADDON_PORT
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
-
-console.log(process.env.MYSQL_ADDON_PORT)
 
 // module.exports = {
 //   query: (text, params) => pool.promise().query(text, params),

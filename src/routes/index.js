1a4d2bd6174814500fcb0b714b76e1ddd5502785
Roger Santos <roger.santos36@gmail.com>
2024-02-06 12:04:53 -0300

    changes...

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,10 @@ const router = express.Router();
 const restaurantsRoutes = require("./restaurants.routes");
 const dealsRoutes = require("./deals.routes");
 
-router.use("/api", restaurantsRoutes);
-router.use("/api", dealsRoutes);
+router.use(restaurantsRoutes);
+router.use(dealsRoutes);
 
-router.get("/", (request, response) => {
+router.use("/", (request, response) => {
   response.status(200).send({
     message: "Rota acessada com sucesso!",
   });

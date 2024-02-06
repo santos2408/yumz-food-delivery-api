b7b9229a1ea37431fbd0aaa1d25f2a0c62e21a24
Roger Santos <roger.santos36@gmail.com>
2024-02-06 14:10:20 -0300

    config deploy

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,13 +5,13 @@ const router = express.Router();
 const restaurantsRoutes = require("./restaurants.routes");
 const dealsRoutes = require("./deals.routes");
 
-// router.use("/api", restaurantsRoutes);
-// router.use("/api", dealsRoutes);
+router.use("/api", restaurantsRoutes);
+router.use("/api", dealsRoutes);
 
-router.get("/", (request, response) => {
-  response.status(200).send({
-    message: "Rota acessada com sucesso!",
-  });
-});
+// router.get("/", (request, response) => {
+//   response.status(200).send({
+//     message: "Rota acessada com sucesso!",
+//   });
+// });
 
 module.exports = router;

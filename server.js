ad46cd9b462088cde9338ed1e24c6f892fc8592f
Roger Santos <roger.santos36@gmail.com>
2024-02-06 11:57:35 -0300

    '.env' config file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 
 dotenv.config()
 
-const port = process.env.MYSQL_ADDON_PORT || 3000;
+const port = process.env.DB_PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Yumz! Food Delivery API executando na porta: ${port}`);
